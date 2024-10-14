1201c890b3c36f1ccd89efeaa612787e1973c185
Rajesh Kamireddy <rajesh17mca@gmail.com>
2024-10-14 00:22:37 -0700

    Updating the application code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,12 +6,18 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello This is simnple Java application to test the Jenkins application!";
+    private final String message = "Hello This is simnple Java application to test the Jenkins application by Printing mathematical table!";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+    }
+
+    private void printTable(int number){
+        for(int i=1;i<=10,i++){
+            System.out.println(Integer.toString(number) + "*" + Integer.toString(i) + "=" + Integer.toString(i*number))
+        }
     }
 
     private final String getMessage() {
